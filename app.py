6f906e97bf71c4060c392f32f4838d4b274f1c6f
“clara-almeida” <clara.almeidagom@gmail.com>
2024-03-20 22:22:51 -0300

    Teste 2

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+import requests
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,3 +11,12 @@ def index():
 @app.route("/teste")
 def teste():
   return "Essa página é um <b>teste</b>" 
+
+@app.route("/telegram", methods=["POST"])
+def telegram_update():
+    update = request.json
+    url_envio_mensagem = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    chat_id = update ["message"]["chat"]["id"]
+    mensagem = {"chat_id": chat_id, "text": "mensagem <b> recebida</b>!", "parse_mode": "HTML"}
+    requests.post(url_envio_mensagem, data=mensagem)
+    return "ok" 
